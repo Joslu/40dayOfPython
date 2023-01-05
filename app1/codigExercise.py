3d984a74de93e0dca1298e26af472187adee63bd
Luis Cruz <luiscru.06@gmail.com>
2023-01-05 16:01:36 -0600

    Day 1

---
+++ app1/codigExercise.py
@@ -0,0 +1,21 @@
+# Coding Exercise 1
+#
+# 1. Create a variable (any name) and store a string in the variable.
+#
+# 2. Then, print out the type of the variable.
+
+
+my_variable = "Value"
+print(type(my_variable))
+
+
+# Coding Exercise 2
+# Write a Python program that gets input from the user and prints out that input.
+user_input = input("Hi, tell me something")
+print(user_input)
+
+# Coding Exercise 3
+# Create a list of four strings and associate the list with a variable.
+
+my_list = ['apple', 'onion', 'watermelon', 'orange']
+
